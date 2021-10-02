7a203347fa164c8f175c60b0b71093fbc3a2b0ea
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-02 17:14:05 +0530

    groups done

--- main/templatetags/extra.py
+++ main/templatetags/extra.py
@@ -4,3 +4,17 @@ register = template.Library()
 @register.filter(name='range')
 def _range(value: int):
     return range(value)
+
+@register.filter(name='filter_users')
+def filter_users(queryset, value: int):
+    length = queryset.count()
+    if value > length:
+        return False
+    return queryset[value-1]
+
+@register.filter(name='filter_users_id')
+def filter_users_id(queryset, value: int):
+    length = queryset.count()
+    if value > length:
+        return False
+    return queryset[value-1].unique_id
