4be459730899c6d2e8589929d913b2cce392acc9
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-20 14:46:59 +0530

    updated the orders page and some decorators

--- payments/decorators.py
+++ payments/decorators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import PermissionDenied
 
 def verify_entry_for_orders(function):
     def wrap(request, *args, **kwargs):
-        count = request.user.orders.count()
+        count = request.user.orders.filter(payment_status="S").count()
         if count > 0:
             raise PermissionDenied
         return function(request, *args, **kwargs)
