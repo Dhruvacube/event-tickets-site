9437862143341089534579afb47192300c6aaf60
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-20 18:39:19 +0530

    fixed admin meta error

--- announcements/admin.py
+++ announcements/admin.py
@@ -6,11 +6,12 @@ from .models import *
 
 
 def retrivejsfile():
-    if settings.DEBUG:
-        return ("js/richTextEditorAnnouncements.js",)
-    return (
-        "https://tanzanite-lpu.github.io/tgl-2.0.0/announcements/static/js/richTextEditorAnnouncements.js",
-    )
+    # if settings.DEBUG:
+    #     return ("js/richTextEditorAnnouncements.js",)
+    # return (
+    #     "https://tanzanite-lpu.github.io/tgl-2.0.0/announcements/static/js/richTextEditorAnnouncements.js",
+    # )
+    return ("js/richTextEditorAnnouncements.js",)
 
 
 # Register your models here.
