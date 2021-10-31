a3f574ba1fc1887cdded0da1db52e16126f96a34
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-31 13:29:27 +0530

    decoratoers update

--- main/decorators.py
+++ main/decorators.py
@@ -1,18 +1,17 @@
 from functools import lru_cache
 
 from django.core.exceptions import PermissionDenied
+from functools import wraps
 
 from .models import GameGroup
 
 
 @lru_cache(maxsize=5)
 def verify_entry_to_group(function):
+    @wraps(function)
     def wrap(request, *args, **kwargs):
         count = GameGroup.objects.filter(users__in=[request.user]).count()
         if count > 0:
             return function(request, *args, **kwargs)
         raise PermissionDenied
-
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
     return wrap
--- payments/decorators.py
+++ payments/decorators.py
@@ -1,25 +1,21 @@
 from django.core.exceptions import PermissionDenied
-
+from functools import wraps
 
 def verify_entry_for_orders(function):
+    @wraps(function)
     def wrap(request, *args, **kwargs):
         count = request.user.orders.filter(payment_status="S").count()
         if count > 0:
             raise PermissionDenied
         return function(request, *args, **kwargs)
-
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
     return wrap
 
 
 def verify_entry_for_payments_history(function):
+    @wraps(function)
     def wrap(request, *args, **kwargs):
         count = request.user.orders.count()
         if count > 0:
             return function(request, *args, **kwargs)
         raise PermissionDenied
-
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
     return wrap
