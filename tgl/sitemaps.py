1cd76faea563b406254c950df52d6b588725955b
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-11-03 14:16:37 +0530

    created sitemap.xml

---
+++ tgl/sitemaps.py
@@ -0,0 +1,24 @@
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import reverse
+
+from main.models import Games
+
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "never"
+
+    @staticmethod
+    def priority(item):
+        return 1.0 if item[0] in ("H", "s", "R") else 0.80
+
+    def items(self):
+        games, l = Games.objects.values("id").iterator(), []
+        for i in games:
+            l.extend([f'about_game/{game_id["id"]}' for game_id in games])
+        return ["home", "signin", "signout", "signup"] + l
+
+    def location(self, item):
+        if item in ("home", "signin", "signout", "signup"):
+            return reverse(item)
+        else:
+            return reverse(item.split('/')[0], args=[item.split('/')[-1]])
--- tgl/urls.py
+++ tgl/urls.py
@@ -2,16 +2,22 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
 from announcements.views import *
 from main.views import *
+
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {"static": StaticViewSitemap}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("accounts.urls")),
     path("accounts/", include("accounts.urls")),
     path("payments/", include("payments.urls")),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="Sitemap"),
     path("", home, name="home"),
     path("make_groups/", group_make, name="make_groups"),
     path("about_game/<int:game_id>", view_games, name="about_game"),
