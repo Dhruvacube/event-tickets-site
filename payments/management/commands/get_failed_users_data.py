5c390b74e033ef2312979ca1980b20654c3218e8
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-11-23 12:13:44 +0530

    command update

--- payments/management/commands/get_failed_users_data.py
+++ payments/management/commands/get_failed_users_data.py
@@ -20,9 +20,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             users_iterator1 = User.objects.filter(
-                orders__payment_status__in=["P", "F"]).values(
+                orders__payment_status__in=["P", "F"],is_staff=False).values(
                     "first_name", "last_name", "university_name", "phone")
-            users_iterator2 = User.objects.filter(orders=None).values(
+            users_iterator2 = User.objects.filter(orders=None,is_staff=False).values(
                 "first_name", "last_name", "university_name", "phone")
             dump_data = [{
                 "First Name": i.get("first_name"),
